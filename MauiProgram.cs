c5371b62ed93ded32f68861b45818eb49604a224
Alan de Oliveira Gonçalves <alandeog@poli.ufrj.br>
2025-07-16 00:16:09 -0300

    Update project

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -15,6 +15,7 @@ public static class MauiProgram
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
 
         // Registrar serviços
